00795398c83a48485cf5237507b81e392e50e081
Svyatoslav Frolov <slava.frolov.614@gmail.com>
2023-01-13 14:24:41 +0300

    huinya

--- chess_project/main.py
+++ chess_project/main.py
@@ -8,6 +8,6 @@ while (1):
     from_coord = coords[0]
     to_coord = coords[1]
     print(table[from_coord[0]][from_coord[1]], table[to_coord[0]][to_coord[1]], sep="\n")
-    table[from_coord], table[to_coord] == table[to_coord], table[from_coord]
+    
     print_table(table)
 
